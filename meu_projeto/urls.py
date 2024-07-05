65b75e8be1cc0adf033dc18ef5e840850f0c94e8
Renatommendes <129893894+Renatommendes@users.noreply.github.com>
2024-07-05 11:00:47 -0300

    Delete meu_projeto directory

--- meu_projeto/urls.py
+++
@@ -1,8 +0,0 @@
-# meu_projeto/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mapapp.urls')),
-]
